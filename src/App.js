2be2c6fe9c4c904000b94f78d539bb55c5489271
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-04 10:43:45 +0300

    sturc img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './common.css';
-import './App.css';
+import logo from './assets/logo.svg';
+import './style/common.css';
+import './style/App.css';
 
 // funkcinis komponentas yra funkcija prasideda diziaja raide
 // grazina jsx
 function MainBtn() {
   const username = 'James';
-  console.log('username ===', username);
+  // console.log('username ===', username);
   return <button>My name is {username}</button>;
 }
 
@@ -15,9 +15,11 @@ function App() {
   const formatedDate = now.toLocaleString('LT-lt', { dateStyle: 'full' });
   return (
     <div className='App container'>
+      <img height='100' src={logo} alt='logotipas' />
       <h1>Hello react !!!</h1>
       <h2>Today is {formatedDate}</h2>
       <MainBtn />
+      <img src='/img/diver.jpg' alt='logo' />
     </div>
   );
 }
