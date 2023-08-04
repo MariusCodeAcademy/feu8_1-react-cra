aff237b51a18f04d1a6b90a516aa4b155a07eb47
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-04 11:41:24 +0300

    1 component per file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 // import logo from './assets/logo.svg';
 import './style/common.css';
 import './style/App.css';
-
-// funkcinis komponentas yra funkcija prasideda diziaja raide
-// grazina jsx
-function MainBtn() {
-  return <button className='btn'>Buy tickets</button>;
-}
-
-function Card1() {
-  const cityName = 'New York!!!';
-  return (
-    <article className='singleCard'>
-      <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
-      <div className='cardInfo'>
-        <h3 className='cardTitle'>{cityName}</h3>
-        <p className='cardDate'>Fri 27 Nov 2016</p>
-        <p className='text'>
-          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
-        </p>
-        <MainBtn />
-      </div>
-    </article>
-  );
-}
+import MainBtn from './components/MainBtn';
+import Card1 from './components/Card1';
 
 function Card2() {
   const cityName = 'Paris !!!';
---
+++ src/components/Card1.jsx
@@ -0,0 +1,20 @@
+import MainBtn from './MainBtn';
+
+function Card1() {
+  const cityName = 'New York!!!';
+  return (
+    <article className='singleCard'>
+      <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
+      <div className='cardInfo'>
+        <h3 className='cardTitle'>{cityName}</h3>
+        <p className='cardDate'>Fri 27 Nov 2016</p>
+        <p className='text'>
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+        </p>
+        <MainBtn />
+      </div>
+    </article>
+  );
+}
+
+export default Card1;
