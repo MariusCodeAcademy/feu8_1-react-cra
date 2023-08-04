20ba752c99fbfece8e52d9c9251374c89f2f2d89
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-04 10:34:12 +0300

    MainBtn formated date

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './common.css';
 import './App.css';
 
+// funkcinis komponentas yra funkcija prasideda diziaja raide
+// grazina jsx
+function MainBtn() {
+  const username = 'James';
+  console.log('username ===', username);
+  return <button>My name is {username}</button>;
+}
+
 function App() {
+  const now = new Date();
+  const formatedDate = now.toLocaleString('LT-lt', { dateStyle: 'full' });
   return (
     <div className='App container'>
       <h1>Hello react !!!</h1>
+      <h2>Today is {formatedDate}</h2>
+      <MainBtn />
     </div>
   );
 }
