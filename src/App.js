fa739775541b6df78458d3ebb706e2eaf8661f7d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-04 11:32:33 +0300

    card btns

--- src/App.js
+++ src/App.js
@@ -5,23 +5,55 @@ import './style/App.css';
 // funkcinis komponentas yra funkcija prasideda diziaja raide
 // grazina jsx
 function MainBtn() {
-  const username = 'James';
-  // console.log('username ===', username);
-  return <button>My name is {username}</button>;
+  return <button className='btn'>Buy tickets</button>;
 }
 
 function Card1() {
-  const cityName = 'New York';
+  const cityName = 'New York!!!';
   return (
     <article className='singleCard'>
       <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
       <div className='cardInfo'>
-        <h3 className='cardTitle'>New York</h3>
+        <h3 className='cardTitle'>{cityName}</h3>
         <p className='cardDate'>Fri 27 Nov 2016</p>
         <p className='text'>
           Praesent tincidunt sed tellus ut rutrum sed vitae justo.
         </p>
-        <button className='btn'>Buy tickets</button>
+        <MainBtn />
+      </div>
+    </article>
+  );
+}
+
+function Card2() {
+  const cityName = 'Paris !!!';
+  return (
+    <article className='singleCard'>
+      <img src='/img/paris.jpg' alt='new york' className='cardImg' />
+      <div className='cardInfo'>
+        <h3 className='cardTitle'>{cityName}</h3>
+        <p className='cardDate'>Fri 17 Nov 2017</p>
+        <p className='text'>
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+        </p>
+        <MainBtn />
+      </div>
+    </article>
+  );
+}
+
+function Card3() {
+  const cityName = 'San Francisco !!!';
+  return (
+    <article className='singleCard'>
+      <img src='/img/sanfran.jpg' alt='new york' className='cardImg' />
+      <div className='cardInfo'>
+        <h3 className='cardTitle'>{cityName}</h3>
+        <p className='cardDate'>Fri 17 Nov 2017</p>
+        <p className='text'>
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+        </p>
+        <MainBtn />
       </div>
     </article>
   );
@@ -34,21 +66,8 @@ function App() {
     <div className='App container'>
       <div className='cardsList grid3'>
         <Card1 />
-        {/* Card2 */}
-        {/* Card3 */}
-        {/* single card START */}
-        <article className='singleCard'>
-          <img src='/img/paris.jpg' alt='new york' className='cardImg' />
-          <div className='cardInfo'>
-            <h3 className='cardTitle'>Paris</h3>
-            <p className='cardDate'>Fri 17 Nov 2017</p>
-            <p className='text'>
-              Praesent tincidunt sed tellus ut rutrum sed vitae justo.
-            </p>
-            <button className='btn'>Buy tickets</button>
-          </div>
-        </article>
-        {/* single card END */}
+        <Card2 />
+        <Card3 />
       </div>
     </div>
   );
