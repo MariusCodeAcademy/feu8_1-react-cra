a340d2a353614a527ed8ffb6d616991041c3a36e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-04 11:23:12 +0300

    component Card1

--- src/App.js
+++ src/App.js
@@ -10,23 +10,45 @@ function MainBtn() {
   return <button>My name is {username}</button>;
 }
 
+function Card1() {
+  const cityName = 'New York';
+  return (
+    <article className='singleCard'>
+      <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
+      <div className='cardInfo'>
+        <h3 className='cardTitle'>New York</h3>
+        <p className='cardDate'>Fri 27 Nov 2016</p>
+        <p className='text'>
+          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
+        </p>
+        <button className='btn'>Buy tickets</button>
+      </div>
+    </article>
+  );
+}
+
 function App() {
   // const now = new Date();
   // const formatedDate = now.toLocaleString('LT-lt', { dateStyle: 'full' });
   return (
     <div className='App container'>
       <div className='cardsList grid3'>
+        <Card1 />
+        {/* Card2 */}
+        {/* Card3 */}
+        {/* single card START */}
         <article className='singleCard'>
-          <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
+          <img src='/img/paris.jpg' alt='new york' className='cardImg' />
           <div className='cardInfo'>
-            <h3 className='cardTitle'>New York</h3>
-            <p className='cardDate'>Fri 27 Nov 2016</p>
+            <h3 className='cardTitle'>Paris</h3>
+            <p className='cardDate'>Fri 17 Nov 2017</p>
             <p className='text'>
               Praesent tincidunt sed tellus ut rutrum sed vitae justo.
             </p>
             <button className='btn'>Buy tickets</button>
           </div>
         </article>
+        {/* single card END */}
       </div>
     </div>
   );
